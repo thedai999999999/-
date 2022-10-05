90a71d96c4564775f1cf782cd15fda8f32c6e343
thedai999999999 <115057708+thedai999999999@users.noreply.github.com>
2022-10-05 11:47:02 +0800

    ???

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc,char *argy[]){
+	int a ;
+ scanf("%d",&a);
+ switch(a/10){ 
+ 	case 10:
+ 	case 9:
+ 	    printf("A");
+ 	    break;
+ 	case 8:
+ 	    printf("B");
+ 	    break;
+ 	case 7:
+ 	    printf("C");
+ 	    break;
+ 	case 6:
+ 	    printf("D");
+ 	    break;
+ 	default:
+ 	    printf("E");
+  	    break;
+ }
+	return 0;
+}
